e766c78f82861d6a72b48bbae77d92f51cb70030
alex.deeplearn <alex.deeplearn@gmail.com>
2020-05-21 19:34:28 -0700

    WIP: Support IP SAN certificates, add default shared runner service

--- util/ssl-cert-generate.sh
+++ util/ssl-cert-generate.sh
@@ -5,5 +5,12 @@ if [ -z "$1" ]; then
 	subj=localhost
 fi	
 
-openssl req -x509 -newkey rsa:4096 -nodes -subj "/CN=${subj}" -keyout ssl.key -out ssl.crt -days 1095
+#openssl req -x509 -newkey rsa:4096 -nodes -subj "/CN=${subj}" -keyout ssl.key -out ssl.crt -days 1095
+
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3560 -nodes -keyout ssl.key -out ssl.crt -subj "/CN=${subj}" -extensions san \
+-config <( \
+  echo '[req]'; \
+  echo 'distinguished_name=req'; \
+  echo '[san]'; \
+  echo 'subjectAltName=DNS:10.0.2.15,IP:10.0.2.15,DNS:localhost')
 
